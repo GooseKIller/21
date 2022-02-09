2b72a80c02ebed8ad3fe50485aee0ef8a89eadb2
GooseKIller <72573175+GooseKIller@users.noreply.github.com>
2022-02-09 16:12:19 +0300

    Update 21.cpp

--- 21.cpp
+++ 21.cpp
@@ -3,47 +3,42 @@
 
 using namespace std;
 
+int choise(int you,int tout) {
+	srand(time(NULL));
+	int card;
+	switch (you)
+	{
+	case 0:
+		card = rand() % 10 + 6;
+		return 0;
+		break;
+	case 1:
+		card = rand() % 10 + 6;
+		tout = tout + card;
+		return tout;
+		break;
+	default:
+		cout << "wrong command";
+		break;
+	}
+}
 int main()
 {   
     cout << "21 GAME\n\ngame rules:\n 1-take a card\n 0-don`t take a card\n";
     srand(time(NULL));
     int you = 0,youint,ai=0,card,round=0,airand=0;
-	while (ai!=21 || you!=21)//rounds
+	while (ai<=21 || you<=21)//rounds
 	{	
 		cout << "ROUND:" << round+1 << "/4\n";
 	if (round == 3) {
 			break;
 		}
 		cout << "YOU:"; cin >> youint;//you
-		switch (youint)
-		{	
-		case 0:
-			cout <<"YOUR card:"<< card << "\n";
-			break;
-		case 1:
-			card = rand() % 9+6;
-			cout << "YOUR card:" << card << "\n";
-			you = you + card;
-			break;
-		default:
-			cout << "wrong command";
-			break;
-		}
+		you=choise(youint, you);
 		cout <<"YOU:" << you<<"\n";
 		airand = rand() % 3;
 		cout << "COMPUTER:"<<airand<< "\n";//compurter
-		switch (airand)
-		{
-		case 0:
-			break;
-		case 1:
-			card = rand() % 9+6;
-			ai = ai + card;
-			break;
-		default:
-			cout << "wrong command";
-			break;
-		}
+		ai = choise(airand, ai);
 		cout << "COMPUTER:" << ai<<"\n";
 		round++;
 
