032cfebb53e6aba7df3c75f80dfa836f3026155c
GooseKIller <72573175+GooseKIller@users.noreply.github.com>
2022-03-01 21:12:50 +0300

    Update 21.cpp

--- 21.cpp
+++ 21.cpp
@@ -32,18 +32,18 @@ int main()
     cout << "21 GAME\n\ngame rules:\n 1-take a card\n 0-don`t take a card\n";
     srand(time(NULL));
     int you = 0,youint,ai=0,card,round=0,airand=0;
-	while (ai<=21 || you<=21)//rounds
+	while (ai<=21 || you<=21)
 	{	
-		cout << "ROUND:" << round+1 << "/4\n";
+	cout << "ROUND:" << round+1 << "/4\n";
 	if (round == 3) {
 			break;
 		}
-		cout << "YOU:"; cin >> youint;//you
+		cout << "YOU:"; cin >> youint;
 		you=choise(youint, you);
 		cout <<"YOU:" << you<<"\n";
 		delay(2000);
 		airand = rand() % 3;
-		cout << "COMPUTER:"<<airand<< "\n";//compurter
+		cout << "COMPUTER:"<<airand<< "\n";
 		ai = choise(airand, ai);
 		delay(2000);
 		cout << "COMPUTER:" << ai<<"\n";
